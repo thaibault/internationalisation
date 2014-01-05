842a44c00d0f460020cf91e426011baeea91bb47
torben <t.sickert@gmail.com>
2014-01-05 18:36:46 +0100

    Update project homepage content.

--- distributionBundle/jquery-tools-1.0.js
+++ distributionBundle/jquery-tools-1.0.js
@@ -150,8 +150,12 @@ Version
         }
         if (!this.self.prototype._javaScriptDependentContentHandled) {
           this.self.prototype._javaScriptDependentContentHandled = true;
-          $(this._defaultOptions.domNodeSelectorPrefix + ' ' + this._defaultOptions.domNodes.hideJavaScriptEnabled).hide();
-          $(this._defaultOptions.domNodeSelectorPrefix + ' ' + this._defaultOptions.domNodes.showJavaScriptEnabled).show();
+          $(this._defaultOptions.domNodeSelectorPrefix + ' ' + this._defaultOptions.domNodes.hideJavaScriptEnabled).filter(function() {
+            return !$(this).data('javaScriptDependentContentHide');
+          }).data('javaScriptDependentContentHide', true).hide();
+          $(this._defaultOptions.domNodeSelectorPrefix + ' ' + this._defaultOptions.domNodes.showJavaScriptEnabled).filter(function() {
+            return !$(this).data('javaScriptDependentContentShow');
+          }).data('javaScriptDependentContentShow', true).show();
         }
         return this;
       }
