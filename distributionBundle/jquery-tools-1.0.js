d390d280e448a2335d3d69efc89258b6a2cf2ec9
torben <t.sickert@gmail.com>
2014-09-23 15:51:26 +0200

    Update project homepage content.

--- distributionBundle/jquery-tools-1.0.js
+++ distributionBundle/jquery-tools-1.0.js
@@ -298,14 +298,6 @@ Version
         
             **returns {$.Tools}**    - Returns the current instance.
          */
-
-        /*
-            NOTE: The "window.setTimeout()" wrapper guarantees that the
-            following function will be executed without any context
-            switches in all browsers.
-            If you want to understand more about that,
-            "What are event loops?" might be a good question.
-         */
         if (this._locks[description] != null) {
           if (this._locks[description].length) {
             this._locks[description].shift()(description);
