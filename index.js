37a55a497aef8705a31aeef1c87a00150ff70706
torben <t.sickert[~at~]gmail.com>
2019-02-04 10:47:54 +0100

    Automatic update to new version.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 'use strict'
 /* !
     region header
-    [Project page](https://torben.website/language)
+    [Project page](https://torben.website/internationalisation)
 
     Copyright Torben Sickert (info["~at~"]torben.website) 16.12.2012
 
