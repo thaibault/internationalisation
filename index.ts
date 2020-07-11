e094d0ecfc48bf22dfd75f90293f18e14707f79f
torben <t.sickert[~at~]gmail.com>
2020-07-11 21:23:04 +0200

    Automatic update to new version.

--- index.ts
+++ index.ts
@@ -138,40 +138,42 @@ export class Internationalisation <TElement extends HTMLElement = HTMLElement>
         this._$domNodeToFade = $domNodeToFade
         this._replacements = replacements
         this._textNodesWithKnownTranslation = textNodesWithKnownTranslation
-        this._options = {
-            domNodeSelectorPrefix: 'body',
-            default: 'enUS',
-            selection: [],
-            initial: null,
-            templateDelimiter: {pre: '{{', post: '}}'},
-            fadeEffect: true,
-            textNodeParent: {
-                showAnimation: [{opacity: 1}, {duration: 'fast'}],
-                hideAnimation: [{opacity: 0}, {duration: 'fast'}]
-            },
-            preReplacementLanguagePattern: '^\\|({1})$',
-            replacementLanguagePattern: '^([a-z]{2}[A-Z]{2}):((.|\\s)*)$',
-            currentLanguagePattern: '^[a-z]{2}[A-Z]{2}$',
-            replacementDomNodeName: ['#comment', 'langreplacement'],
-            replaceDomNodeNames: ['#text', 'langreplace'],
-            toolsLockDescription: '{1}Switch',
-            languageHashPrefix: 'language-',
-            currentLanguageIndicatorClassName: 'current',
-            sessionDescription: '{1}',
-            languageMapping: {
-                deDE: ['de', 'de_de', 'de-de', 'german', 'deutsch'],
-                enUS: ['en', 'en_us', 'en-us'],
-                enEN: ['en_en', 'en-en', 'english'],
-                frFR: ['fr', 'fr_fr', 'fr-fr', 'french']
-            },
-            onSwitched: Tools.noop,
-            onEnsured: Tools.noop,
-            onSwitch: Tools.noop,
-            onEnsure: Tools.noop,
-            domNode: {knownTranslation: 'div.toc'}
-        }
         // endregion
-        super.initialize(options)
+        super.initialize(Tools.extend(
+            true,
+            {
+                currentLanguageIndicatorClassName: 'current',
+                currentLanguagePattern: '^[a-z]{2}[A-Z]{2}$',
+                default: 'enUS',
+                domNode: {knownTranslation: 'div.toc'},
+                fadeEffect: true,
+                initial: null,
+                languageHashPrefix: 'language-',
+                languageMapping: {
+                    deDE: ['de', 'de_de', 'de-de', 'german', 'deutsch'],
+                    enUS: ['en', 'en_us', 'en-us'],
+                    enEN: ['en_en', 'en-en', 'english'],
+                    frFR: ['fr', 'fr_fr', 'fr-fr', 'french']
+                },
+                onSwitched: Tools.noop,
+                onEnsured: Tools.noop,
+                onSwitch: Tools.noop,
+                onEnsure: Tools.noop,
+                preReplacementLanguagePattern: '^\\|({1})$',
+                replaceDomNodeNames: ['#text', 'langreplace'],
+                replacementDomNodeName: ['#comment', 'langreplacement'],
+                replacementLanguagePattern: '^([a-z]{2}[A-Z]{2}):((.|\\s)*)$',
+                selection: [],
+                sessionDescription: '{1}',
+                templateDelimiter: {pre: '{{', post: '}}'},
+                textNodeParent: {
+                    hideAnimation: [{opacity: 0}, {duration: 'fast'}],
+                    showAnimation: [{opacity: 1}, {duration: 'fast'}]
+                },
+                toolsLockDescription: '{1}Switch'
+            },
+            options
+        ))
         this._options.preReplacementLanguagePattern = Tools.stringFormat(
             this._options.preReplacementLanguagePattern,
             this._options.replacementLanguagePattern.substr(
